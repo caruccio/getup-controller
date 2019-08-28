10a2477a3f30e2ff8913b66a3044af0c24d8b98f
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2019-08-28 15:13:04 -0300

    Replace tab for spaces

--- app/project-reconciler.py
+++ app/project-reconciler.py
@@ -10,81 +10,81 @@ import requests
 
 
 def log(*vargs, **kwargs):
-	print(file=sys.stderr, *vargs, **kwargs)
+    print(file=sys.stderr, *vargs, **kwargs)
 
 
 def load_config():
-	config_file = os.environ.get("CONTROLLER_CONFIG", "/config/controller.yaml")
-	config = {}
-	err = False
+    config_file = os.environ.get("CONTROLLER_CONFIG", "/config/controller.yaml")
+    config = {}
+    err = False
 
-	try:
-		if os.path.exists(config_file):
-			with open(config_file, 'r') as cf:
-				config = yaml.safe_load(cf)
-#				log('Loaded config from', config_file)
-	except Exception as ex:
-		log("Error loading config: %s: %s" % (config_file, str(ex)))
-		err = True
+    try:
+        if os.path.exists(config_file):
+            with open(config_file, 'r') as cf:
+                config = yaml.safe_load(cf)
+#                log('Loaded config from', config_file)
+    except Exception as ex:
+        log("Error loading config: %s: %s" % (config_file, str(ex)))
+        err = True
 
-	return config, err
+    return config, err
 
 
 def main():
-	config, err = load_config()
-	if err:
-		return
+    config, err = load_config()
+    if err:
+        return
 
-	ignore_namespaces = config.get('ignore_namespaces', ['default', 'kube-system'])
-	state = json.load(sys.stdin)
-	metadata = state.get('object',{}).get('metadata',{})
-	annotations = metadata.get('annotations',{})
+    ignore_namespaces = config.get('ignore_namespaces', ['default', 'kube-system'])
+    state = json.load(sys.stdin)
+    metadata = state.get('object',{}).get('metadata',{})
+    annotations = metadata.get('annotations',{})
 
-	name = metadata.get('name')
+    name = metadata.get('name')
 
-	if ignore_namespaces and name in ignore_namespaces:
-		log("Namespace ignored")
-		print_response(True)
-		return
+    if ignore_namespaces and name in ignore_namespaces:
+        log("Namespace ignored")
+        print_response(True)
+        return
 
-	owner = annotations.get('getup.io/owner') or annotations.get('openshift.io/requester')
+    owner = annotations.get('getup.io/owner') or annotations.get('openshift.io/requester')
 
-	if owner is None:
-		log("Namespace %s is missing ownership annotation: either \"getup.io/owner\" or \"openshift.io/requester\" must be supplied." % name)
-		return
+    if owner is None:
+        log("Namespace %s is missing ownership annotation: either \"getup.io/owner\" or \"openshift.io/requester\" must be supplied." % name)
+        return
 
-	if config.get('username_type') == 'email' and '@' not in owner:
-		log("Invalid annotation: owner must be an email address.")
-		return
+    if config.get('username_type') == 'email' and '@' not in owner:
+        log("Invalid annotation: owner must be an email address.")
+        return
 
-	log('Project "%s" owner sould be "%s"' % (name, owner))
+    log('Project "%s" owner sould be "%s"' % (name, owner))
 
-	project_list_url = os.environ["GETUP_API_URL"].strip('/') + '/api/v1/project/'
-	project_url = project_list_url + name + '/'
-	auth = (os.environ["GETUP_API_USERNAME"], os.environ["GETUP_API_PASSWORD"])
+    project_list_url = os.environ["GETUP_API_URL"].strip('/') + '/api/v1/project/'
+    project_url = project_list_url + name + '/'
+    auth = (os.environ["GETUP_API_USERNAME"], os.environ["GETUP_API_PASSWORD"])
 
-	res = requests.get(project_url, auth=auth, params={"status":"active"}, allow_redirects=True)
-	project = res.json()
+    res = requests.get(project_url, auth=auth, params={"status":"active"}, allow_redirects=True)
+    project = res.json()
 
-	if res.status_code == 200:
-		if project['owner'] == owner:
-			log('Project "%s" already owned by "%s".' % (name, owner))
-		else:
-			log('Conflict: project "%s" owned by "%s", but namespace says it should be owned by "%s"' % (name, project['owner'], owner))
-		return
+    if res.status_code == 200:
+        if project['owner'] == owner:
+            log('Project "%s" already owned by "%s".' % (name, owner))
+        else:
+            log('Conflict: project "%s" owned by "%s", but namespace says it should be owned by "%s"' % (name, project['owner'], owner))
+        return
 
-	if res.status_code == 404:
-		data = {'owner':owner, 'name':name, 'family':'default'}
-		log('Will create project with "%s"' % data)
-		res = requests.post(project_list_url, auth=auth, data=data, params={'sync':True})
-		log('Project create "%s" returned: %s %s' % (name, res.status_code, res.reason))
+    if res.status_code == 404:
+        data = {'owner':owner, 'name':name, 'family':'default'}
+        log('Will create project with "%s"' % data)
+        res = requests.post(project_list_url, auth=auth, data=data, params={'sync':True})
+        log('Project create "%s" returned: %s %s' % (name, res.status_code, res.reason))
 
-		if not res.ok:
-			log('Error creating billing Project:', res.text)
+        if not res.ok:
+            log('Error creating billing Project:', res.text)
 
 if __name__ == "__main__":
-	try:
-		main()
-	except Exception as e:
-		logging.exception("%s", e)
-		sys.exit(1)
+    try:
+        main()
+    except Exception as e:
+        logging.exception("%s", e)
+        sys.exit(1)
--- app/project-validator.py
+++ app/project-validator.py
@@ -10,76 +10,76 @@ import logging
 import requests
 
 def log(*vargs, **kwargs):
-	print(file=sys.stderr, *vargs, **kwargs)
+    print(file=sys.stderr, *vargs, **kwargs)
 
 
 def print_response(allowed, reason='', code=200):
-	res = {
-		"allowed": allowed,
-		"status": {
-			"code": code,
-			"reason": reason
-		}
-	}
+    res = {
+        "allowed": allowed,
+        "status": {
+            "code": code,
+            "reason": reason
+        }
+    }
 
-	json.dump(res, sys.stdout)
+    json.dump(res, sys.stdout)
 
 
 def load_config():
-	config_file = os.environ.get("CONTROLLER_CONFIG", "/config/controller.yaml")
-	config = {}
-	err = False
+    config_file = os.environ.get("CONTROLLER_CONFIG", "/config/controller.yaml")
+    config = {}
+    err = False
 
-	try:
-		if os.path.exists(config_file):
-			with open(config_file, 'r') as cf:
-				config = yaml.safe_load(cf)
-#				log('Loaded config from', config_file)
-	except Exception as ex:
-		log("Error loading config: %s: %s" % (config_file, str(ex)))
-		err = True
+    try:
+        if os.path.exists(config_file):
+            with open(config_file, 'r') as cf:
+                config = yaml.safe_load(cf)
+#                log('Loaded config from', config_file)
+    except Exception as ex:
+        log("Error loading config: %s: %s" % (config_file, str(ex)))
+        err = True
 
-	return config, err
+    return config, err
 
 
 def main():
-	allowed = True
-	reason = "Namespace accepted"
+    allowed = True
+    reason = "Namespace accepted"
 
-	config, err = load_config()
-	if err:
-		print_response(True)
-		return
+    config, err = load_config()
+    if err:
+        print_response(True)
+        return
 
-	ignore_namespaces = config.get('ignore_namespaces', ['default', 'kube-system'])
-	state = json.load(sys.stdin)
-	metadata = state.get('object',{}).get('metadata',{})
-	annotations = metadata.get('annotations',{})
+    ignore_namespaces = config.get('ignore_namespaces', ['default', 'kube-system'])
+    state = json.load(sys.stdin)
+    metadata = state.get('object',{}).get('metadata',{})
+    annotations = metadata.get('annotations',{})
 
-	name = metadata.get('name')
+    name = metadata.get('name')
 
-	if ignore_namespaces and name in ignore_namespaces:
-		log("Namespace ignored")
-		print_response(True)
-		return
+    if ignore_namespaces and name in ignore_namespaces:
+        log("Namespace ignored")
+        print_response(True)
+        return
 
-	owner = annotations.get('getup.io/owner') or annotations.get('openshift.io/requester')
+    owner = annotations.get('getup.io/owner') or annotations.get('openshift.io/requester')
 
-	if owner is None:
-		allowed = False
-		reason = "Missing ownership annotation: either \"getup.io/owner\" or \"openshift.io/requester\" must be supplied."
-	elif config.get('username_type') == 'email' and '@' not in owner:
-		allowed = False
-		reason = "Invalid annotation: owner must be an email address."
+    if owner is None:
+        allowed = False
+        reason = "Missing ownership annotation: either \"getup.io/owner\" or \"openshift.io/requester\" must be supplied."
+    elif config.get('username_type') == 'email' and '@' not in owner:
+        allowed = False
+        reason = "Invalid annotation: owner must be an email address."
 
-	log("{}: {}".format(reason, name))
+    log("{}: {}".format(reason, name))
 
-	print_response(allowed, reason)
+    print_response(allowed, reason)
 
 
 if __name__ == "__main__":
-	try:
-		main()
-	except Exception as e:
-		logging.exception("%s", e)
-		sys.exit(1)
+    try:
+        main()
+    except Exception as e:
+        logging.exception("%s", e)
+        sys.exit(1)
